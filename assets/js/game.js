dcec659accbf90fc9c7b687461d6369ea2284332
Jokernal <jordankernzx@gmail.com>
2022-05-14 12:59:24 -0500

    working with js

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log("our robots name is" + playerName);
+function fight()
+{
+    window.alert("The fight has begun!");
+}
+// fight();
